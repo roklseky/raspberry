376ae67c32187042bf31cccd96cc00033bb09418
roklseky <62904571+roklseky@users.noreply.github.com>
2022-09-25 20:43:48 +0200

    Create piolo480.sh

---
+++ piolo480.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# https://tailscale.com/kb/1019/subnets/
+
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p /etc/sysctl.conf
+
+sudo tailscale up --advertise-routes=10.0.0.0/24 --advertise-exit-node
