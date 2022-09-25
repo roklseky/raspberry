13435865354c3a65a01b2d79847cf753e4ca2b54
roklseky <62904571+roklseky@users.noreply.github.com>
2022-09-25 18:58:19 +0200

    Create ini.sh

---
+++ ini.sh
@@ -0,0 +1,8 @@
+###########################
+# Raspberry Pi imager requires username and password to be configured in "advanced" menu. 
+# Enable SSH
+# Set username and password
+# Set locale setting
+# Always to use
+###########################
+
