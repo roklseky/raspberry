138541455199867d71d0a5eb3c12d817c2d60a5c
roklseky <62904571+roklseky@users.noreply.github.com>
2022-09-30 23:04:55 +0200

    Update piolo480.sh

--- piolo480.sh
+++ piolo480.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-# https://tailscale.com/kb/1019/subnets/
 
+# Static IP /etc/dhcpcd.conf
+echo "interface eth0" | sudo tee -a touch /etc/dhcpcd.conf
+echo "static ip_address=10.0.0.111/24"
+echo "static routers=10.0.0.1"
+echo "static domain_name_servers=10.0.0.1"
+
+
+# https://tailscale.com/kb/1019/subnets/
 echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
 echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p /etc/sysctl.conf
 
-sudo tailscale up --advertise-routes=10.0.0.0/24 --advertise-exit-node
+sudo tailscale up --advertise-routes=10.0.0.0/24 --advertise-exit-node --ssh
