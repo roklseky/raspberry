a83e39104097bf22b5e1c99b2f778f35085a7fc5
roklseky <62904571+roklseky@users.noreply.github.com>
2022-09-30 23:06:45 +0200

    Update piolo480.sh

--- piolo480.sh
+++ piolo480.sh
@@ -11,5 +11,4 @@ echo "static domain_name_servers=10.0.0.1"
 echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
 echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p /etc/sysctl.conf
-
 sudo tailscale up --advertise-routes=10.0.0.0/24 --advertise-exit-node --ssh
